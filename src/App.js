bfede14a6c45085f055e016cf0b2b11dc4c56cca
Rachel <Rachel.Heneault@gmail.com>
2022-01-20 08:18:37 -0500

    Initial commit
    git commit -m Initoia

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import Picker from 'emoji-picker-react'
+
+export default function Emoji(){
+  const [chosenEmoji,setChosenEmoji]=useState(null)
+  const onEmojiClick = (event,emojiObject)=>{
+    setChosenEmoji(emojiObject)
+  }
+  return (
+    <div>
+      <h3>Emoji Picker</h3>
+      {chosenEmoji?( <span>Your Emoji:{chosenEmoji.emoji}</span>):(<span>No Emoji</span>)}
+      <Picker onEmojiClick={onEmojiClick}/>     
+    </div>
+  )
+}
+
